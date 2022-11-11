78b3d501b619fb1c260e2396dfc3b49b3a2c021b
Ganesh Kodaganti <ganesh.kodaganti@techigai.local>
2022-11-11 11:17:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
+    .finally(res => {
+        window.tab = `<tr>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>PHONE</th>
+        <th>WEBSITE</th>
+        <th>COMPANY NAME</th>
+         </tr>`
+    })
+    .then(data => {
+        // console.log(data);
+        window.tr = "";
+        for (let i = 0; i < data.length; ++i) {
+
+            window.tr += `<tr onclick="func(${i})" class="row">
+            <td>${data[i].name}</td>
+            <td>${data[i].email}</td> 
+            <td>${data[i].phone}</td>
+            <td>${data[i].website}</td>
+            <td>${data[i].company.name}</td>
+            `;
+        }
+
+        document.getElementById("table").innerHTML = (tab + tr);
+    })
+
+function func(tp) {
+    alert(`clicked on ${tp}`);
+}
